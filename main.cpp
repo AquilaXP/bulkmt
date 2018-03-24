c08bed41f34ab40eee6b3db80512763f5556f635
Aquila <aquila-2004@yandex.ru>
2018-03-24 20:05:30 +0300

    Тестирование

--- main.cpp
+++ main.cpp
@@ -9,9 +9,10 @@
 #include "ConsoleObserver.h"
 #include "ThreadObserver.h"
 
-
+volatile size_t v = 0;
 int main( int ac, char* av[] )
 {
+    std::cout << "start!\n";
     if( ac != 2 )
     {
         std::cout << "Not set size block command!\n";
@@ -19,17 +20,38 @@ int main( int ac, char* av[] )
     }
     int32_t N = std::atoi(av[1]);
 
-    ConsoleObserver console_obs;
-    ThreadObserver thread_console_obs( &console_obs, 1 );
-    // Запускаем поток коносли
-    thread_console_obs.Start();
+    auto burden = []() ->void
+    {
+        for( size_t i = 0; i < 50000000; ++i )
+            v++;
+    };
 
-    FileObserverMT file_observer( 4, 4 );
-
-    IStreamSubject s( N );
-    s.Attach( &thread_console_obs );
-    s.Attach( &file_observer );
-    s.Run( std::cin );
+    auto b = std::chrono::high_resolution_clock::now();
+    std::chrono::time_point<std::chrono::steady_clock> emain;
+    struct guard_time
+    {
+        guard_time(std::chrono::time_point<std::chrono::steady_clock>& t ):m_t(t)
+        {}
+        ~guard_time()
+        {
+            m_t = std::chrono::high_resolution_clock::now();
+        }
+        std::chrono::time_point<std::chrono::steady_clock>& m_t;
+    };
+    {
+        const size_t count_thread = 1;
+        FileObserverMT file_observer( count_thread, count_thread );
+        file_observer.SetBurden( burden );
+        guard_time gt( emain );
+        IStreamSubject s( N );
+       // s.Attach( &thread_console_obs );
+        s.Attach( &file_observer );
+        std::ifstream f( "test_cmd.txt" );
+        s.Run( f );
+    }
+    auto e = std::chrono::high_resolution_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>( emain - b ).count() << std::endl;
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>( e - b ).count() << std::endl;
 
     return 0;
 }
